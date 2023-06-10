8f01cc26d11d73e94e61b912e0aad034b43dcc0d
Luke Tong <luke@Lukes-MBP-2.fios-router.home>
2023-06-09 21:53:59 -0400

    Added UX through stdin

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,16 @@ mod img;
 mod util;
 
 fn main() {
-	let width = 1024;
-	let height = 1024;
-	img::write_to("examples/mandelbrot.png", (width, height), &fractals::Fractal::Mandelbrot).expect("ERROR: Failed to write to image");
-	img::write_to("examples/sierpinski.png", (width, height), &fractals::Fractal::Sierpinski).expect("ERROR: Failed to write to image");
+	println!("Enter desired width and height in pixels (only enter ONE value): ");
+	let mut line: String = String::new();
+	std::io::stdin()
+		.read_line(&mut line)
+		.expect("Failed to read line");
+	let pixels = line
+		.trim()
+		.parse::<u32>()
+		.expect("FAILED TO READ INT FROM INPUT. TERMINATED");
+
+	img::write_to("examples/mandelbrot.png", (pixels, pixels), &fractals::Fractal::Mandelbrot).expect("ERROR: Failed to write to image");
+	img::write_to("examples/sierpinski.png", (pixels, pixels), &fractals::Fractal::Sierpinski).expect("ERROR: Failed to write to image");
 }
